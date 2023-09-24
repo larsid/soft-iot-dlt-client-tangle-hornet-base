a4408cf83fdfe39cff51e7001b5bd131ac2d746f
AllanCapistrano <allan.capistrano3@gmail.com>
2023-09-24 19:55:52 -0300

    feat: create ILedgerWriter interface

---
+++ src/main/java/br/uefs/larsid/iot/soft/services/ILedgerWriter.java
@@ -0,0 +1,13 @@
+package br.uefs.larsid.iot.soft.services;
+
+import br.uefs.larsid.iot.soft.model.transactions.Transaction;
+
+/**
+ * @author Allan Capistrano
+ * @version 1.0.0
+ */
+public interface ILedgerWriter {
+  public void put(Transaction transaction) throws InterruptedException;
+
+  public void createMessage(String index, String content);
+}
