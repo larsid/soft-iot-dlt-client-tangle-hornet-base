a20ae2ea324e29dde4b4a6941e8f8b654f03e097
AllanCapistrano <allan.capistrano3@gmail.com>
2023-09-24 15:07:18 -0300

    feat: create TransactionType enum

---
+++ src/main/java/br/uefs/larsid/iot/soft/enums/TransactionType.java
@@ -0,0 +1,15 @@
+package br.uefs.larsid.iot.soft.enums;
+
+/**
+ *
+ * @author Uellington Damasceno, Allan Capistrano
+ */
+public enum TransactionType {
+  LB_ENTRY,
+  LB_ENTRY_REPLY,
+  LB_STATUS,
+  LB_REQUEST,
+  LB_REPLY,
+  LB_DEVICE,
+  REP_EVALUATION,
+}
