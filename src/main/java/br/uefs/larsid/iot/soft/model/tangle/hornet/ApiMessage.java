8a465cb55acb2c347da2b38167662d776fd7d07b
AllanCapistrano <allan.capistrano3@gmail.com>
2023-10-03 00:19:37 -0300

    feat: create ApiMessage class

---
+++ src/main/java/br/uefs/larsid/iot/soft/model/tangle/hornet/ApiMessage.java
@@ -0,0 +1,19 @@
+package br.uefs.larsid.iot.soft.model.tangle.hornet;
+
+/**
+ * @author Allan Capistrano
+ * @version 1.0.0
+ */
+public class ApiMessage {
+
+  private String index;
+  private String content;
+
+  public String getIndex() {
+    return index;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
