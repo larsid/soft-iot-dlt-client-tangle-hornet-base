e23bbb15e4f1c24f1a692a0954cae3d6627538e1
AllanCapistrano <allan.capistrano3@gmail.com>
2023-10-02 23:22:33 -0300

    feat: add subscribe and unsubscribe methods

--- src/main/java/br/uefs/larsid/iot/soft/services/ILedgerReader.java
+++ src/main/java/br/uefs/larsid/iot/soft/services/ILedgerReader.java
@@ -1,11 +1,15 @@
 package br.uefs.larsid.iot.soft.services;
 
 /**
- * @author Allan Capistrano
+ * @author Allan Capistrano, Uellington Damasceno
  * @version 1.0.0
  */
 public interface ILedgerReader {
   public String getMessagesByIndex(String index);
 
   public String getMessageByMessageId(String messageId);
+
+  public void subscribe(String topic, ILedgerSubscriber subscriber);
+
+  public void unsubscribe(String topic, ILedgerSubscriber subscriber);
 }
