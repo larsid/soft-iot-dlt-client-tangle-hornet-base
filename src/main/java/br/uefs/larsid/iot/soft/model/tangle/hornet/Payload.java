43479dc9e870de702f0e7374d5afe191695897c1
AllanCapistrano <allan.capistrano3@gmail.com>
2023-10-02 23:18:10 -0300

    feat: create Message and Payload classes

---
+++ src/main/java/br/uefs/larsid/iot/soft/model/tangle/hornet/Payload.java
@@ -0,0 +1,19 @@
+package br.uefs.larsid.iot.soft.model.tangle.hornet;
+
+/**
+ * @author Allan Capistrano
+ * @version 1.0.0
+ */
+public class Payload {
+
+  private String data;
+  private String index;
+
+  public String getData() {
+    return data;
+  }
+
+  public String getIndex() {
+    return index;
+  }
+}
