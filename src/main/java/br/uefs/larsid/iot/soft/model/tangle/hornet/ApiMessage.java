058b78875e1441b5d127fe71e6767817f04f4d5b
AllanCapistrano <allan.capistrano3@gmail.com>
2023-10-04 09:32:55 -0300

    feat: add TODO

--- src/main/java/br/uefs/larsid/iot/soft/model/tangle/hornet/ApiMessage.java
+++ src/main/java/br/uefs/larsid/iot/soft/model/tangle/hornet/ApiMessage.java
@@ -4,7 +4,7 @@ package br.uefs.larsid.iot.soft.model.tangle.hornet;
  * @author Allan Capistrano
  * @version 1.0.0
  */
-public class ApiMessage {
+public class ApiMessage { // TODO: Alterar API para ser 'index' e 'data' e usar a classe 'Payload'
 
   private String index;
   private String content;
