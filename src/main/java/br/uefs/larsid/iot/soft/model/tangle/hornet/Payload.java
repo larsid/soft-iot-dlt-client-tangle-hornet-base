9280fe3e04f5be5be3f14d14ac701c72c8f93ddf
AllanCapistrano <allan.capistrano3@gmail.com>
2023-10-06 09:37:36 -0300

    feat: create stringToPayload method

--- src/main/java/br/uefs/larsid/iot/soft/model/tangle/hornet/Payload.java
+++ src/main/java/br/uefs/larsid/iot/soft/model/tangle/hornet/Payload.java
@@ -1,5 +1,7 @@
 package br.uefs.larsid.iot.soft.model.tangle.hornet;
 
+import com.google.gson.Gson;
+
 /**
  * @author Allan Capistrano
  * @version 1.0.0
@@ -8,6 +10,16 @@ public class Payload {
 
   private String data;
   private String index;
+
+  /**
+   * Converts a string into a Payload object.
+   * 
+   * @param string String - String that will be converted.
+   * @return Payload
+   */
+  public static Payload stringToPayload(String string) {
+    return new Gson().fromJson(string, Payload.class);
+  }
 
   public String getData() {
     return data;
